4848658c9dda5c505712acebfa7b74764e4310ab
Ivan Ermilov <earthquakesan@gmail.com>
2016-11-28 14:09:33 +0100

    added test to ensure the sequence of words from endpoint

--- tests/test_palmetto.py
+++ tests/test_palmetto.py
@@ -18,10 +18,12 @@ def test_get_coherence(capsys, words):
     coherence = palmetto.get_coherence(words)
     assert(coherence == 0.5678879445677241)
 
+
 def test_get_coherence_fast(capsys, words):
     palmetto = Palmetto()
     coherence = palmetto.get_coherence_fast(words)
     assert(coherence == 1779.6591356383024)
+
 
 def test_wrong_endpoint(words):
     palmetto = Palmetto("http://example.com/nothinghere/")
@@ -40,12 +42,21 @@ def test_all_coherence_types(words):
     for coherence_type in palmetto.all_coherence_types:
         palmetto.get_coherence(words, coherence_type=coherence_type)
 
+
 def test_wrong_content_type(words):
     palmetto = Palmetto()
     with pytest.raises(WrongContentType):
         palmetto._request_by_service(words, "cv", "bla")
 
+
 def test_all_content_types(words):
     palmetto = Palmetto()
     for content_type in ["text", "bytes"]:
         palmetto._request_by_service(words, "umass", content_type)
+
+
+def test_get_df_for_words(words):
+    palmetto = Palmetto()
+    doc_ids = palmetto.get_df_for_words(words)
+    for i in range(0, len(words)):
+        assert(doc_ids[i][0] == words[i])
