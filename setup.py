0c1b4a88dc4f44808960b2cc421c24943717c666
Ivan Ermilov <earthquakesan@gmail.com>
2016-12-09 22:31:49 +0100

    v3.0 --> v3.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'palmettopy',
   packages = ['palmettopy'], # this must be the same as the name above
-  version = 'v3.0',
+  version = 'v3.1',
   description = 'Palmetto Python Interface',
   author = 'Ivan Ermilov',
   author_email = 'ivan.s.ermilov@gmail.com',
   url = 'https://github.com/earthquakesan/palmetto-py', # use the URL to the github repo
-  download_url = 'https://github.com/earthquakesan/palmetto-py/archive/v3.0.tar.gz', 
+  download_url = 'https://github.com/earthquakesan/palmetto-py/archive/v3.1.tar.gz', 
   keywords = ['aksw', 'nlp', 'semantic-web'], # arbitrary keywords
   classifiers = [],
   install_requires=[
