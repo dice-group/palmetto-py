d9637fea26df0801343806fe52c065a1bbb4e495
Micha <michael.roeder@uni-paderborn.de>
2021-06-29 11:51:11 +0200

    Increased the version to 3.3.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'palmettopy',
   packages = ['palmettopy'], # this must be the same as the name above
-  version = 'v3.2',
+  version = 'v3.3',
   description = 'Palmetto Python Interface',
   author = 'Ivan Ermilov',
   author_email = 'ivan.s.ermilov@gmail.com',
-  url = 'https://github.com/earthquakesan/palmetto-py', # use the URL to the github repo
-  download_url = 'https://github.com/earthquakesan/palmetto-py/archive/v3.1.tar.gz', 
-  keywords = ['aksw', 'nlp', 'semantic-web'], # arbitrary keywords
+  url = 'https://github.com/dice-group/palmetto-py', # use the URL to the github repo
+  #download_url = 'https://github.com/earthquakesan/palmetto-py/archive/v3.1.tar.gz', 
+  keywords = ['aksw', 'nlp', 'semantic-web', 'dice-research', 'topic modeling', 'topic coherence'], # arbitrary keywords
   classifiers = [],
   install_requires=[
     'requests'
